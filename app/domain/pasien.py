a8cbf08b8a45bc9fc2add58b760889c6ca8401ff
freiazulaika <freiazulaika27@gmail.com>
2026-03-02 00:53:04 +0700

    SB-29 feat: add Pasien domain entity

---
+++ app/domain/pasien.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+
+class Pasien(BaseModel):
+    id_pasien: str
+    nik: str
+    nama: str
+    no_telepon: str
+    alamat: str
+    golongan_darah: str
+    password_hash: str
+    version_id: int = 1
